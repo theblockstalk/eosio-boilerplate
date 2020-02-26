a85571c98845090f92b1049ac41db9c6686284df
Jack Tanner <jack@jackandtheblockstalk.com>
2020-02-27 00:56:21 +0100

    fix

--- scripts/configure_git_server.sh
+++ scripts/configure_git_server.sh
@@ -8,5 +8,5 @@ cd $HOME/project.git
 git init --bare
 git worktree add $HOME/project.git-working-tree master
 
-cp $HOME/eosio-boilerplate/scripts/post-recieve $HOME/project.git/hooks/
+cp $HOME/eosio-boilerplate/scripts/post-receive $HOME/project.git/hooks/
 chmod +x $HOME/project.git/hooks/post-receive
