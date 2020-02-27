ab9bfe4bc10f292321e068309f1f1dbe8dcebd22
Jack Tanner <jack@jackandtheblockstalk.com>
2020-02-27 01:03:57 +0100

    fix

--- scripts/configure_git_local.sh
+++ scripts/configure_git_local.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 eval "$(ssh-agent -s)"
-ssh-keygen -t rsa -b 4096 -C "your_email@example.com" -f keys/ec2-ssh > keys/ec2-ssh.pub
+# ssh-keygen -t rsa -b 4096 -C "your_email@example.com" -f keys/ec2-ssh > keys/ec2-ssh.pub
 ssh-add keys/ec2-ssh
 
 git remote add ec2 ubuntu@ec2-3-9-189-125.eu-west-2.compute.amazonaws.com:/home/ubuntu/project.git/
